186f72c7ea931012c67de19d760322a2c664ec28
Nathaniel-Kerr <NathanielKerr10@gmail.com>
2020-11-24 23:23:47 -0800

    Done.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,7 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative '../lib/scraper.rb'
 require_relative '../lib/info.rb'
@@ -12,29 +13,50 @@ class CLI
     end
 
     def call_to_action
+        puts ""
         puts "Welcome to ACC! Would you like to see our staff? Enter 'yes' or 'no'."
         input = gets.chomp
+
         if input == "yes"
             Info.list_staff
             puts ""
             make_selection
         elsif input == "no"
             "It was a pleasure to meet you!"
-        else call_to_action
+        else 
+            call_to_action
         end 
     end
 
     def make_selection
         puts "Choose a name and enter the number to see position."
         input = gets.chomp.to_i
-        puts "You have picked #{Info.staff[input]} the #{Info.positions[input]}"
-        sleep(2)
+
+        if input > 33 
+            puts ""
+            puts "Invalid Input, Please try again."
+            puts ""
+            sleep(3)
+            Info.list_staff
+            puts ""
+            make_selection
+        else
+            puts ""
+            puts "You have picked #{Info.staff[input]} the #{Info.positions[input]}"
+            sleep(3)
+            go_again
+        end
+    end
+
+    def go_again
         puts ""
         puts "Go again? 'yes' or 'no'.."
         input2 = gets.chomp 
+
         if input2 == "yes"
             CLI.new
         elsif input2 == "no" 
+            puts ""
             puts "Thanks for checking in. BYE!"
         else 
             invalid_input
@@ -42,11 +64,14 @@ class CLI
     end
 
     def invalid_input
+        puts ""
         puts "Invalid Input. Go again? 'yes' - 'no'"
         input = gets.chomp
+
         if input == "yes"
             CLI.new
         else 
+            puts ""
             puts "See ya, Thanks for your time!"
         end
     end
